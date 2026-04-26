7bf82c4a6569d7264903d3c31fe38a9e62f3ccd7
Fitil4ik <fitilok159@gmail.com>
2026-04-26 13:47:42 +0300

    eventManager: implemented error channel and analytics

--- utils/eventManager.js
+++ utils/eventManager.js
@@ -25,4 +25,32 @@ class SafeEventBus extends EventEmitter {
 
 const eventBus = new SafeEventBus();
 
+eventBus.on('error', (err) => {
+    logger.error(`[EventBus] Помилка в одному зі слухачів: ${err.message}`);
+});
+
+eventBus.on('error', (err) => {
+    logger.error(`[EventBus] Помилка в одному зі слухачів: ${err.message}`);
+});
+
+function updateCache({ username, responseData, cache }) {
+    cache.set(username, { data: responseData, timestamp: Date.now() });
+    logger.debug(`[GetCache] Дані для ${username} успішно збережено в кеш.`);
+}
+
+function logAnalytics({ responseData }) {
+    const topGenres = responseData.top_genres;
+    if (Array.isArray(topGenres) && topGenres.length) {
+        logger.debug(`[CalcPref] Жанри за пріоритетом (всього знайдено ${topGenres.length}):`);
+        topGenres.slice(0, 10).forEach(g => {
+            logger.debug(`- ${g.name}: ${g.weightedRank} (Середній: ${g.rawAverage}, кількість: ${g.count})`);
+        });
+    } else {
+        logger.debug(`[CalcPref] Статистика жанрів недоступна.`);
+    }
+}
+
+eventBus.on('profile_ready', updateCache);
+eventBus.on('profile_ready', logAnalytics);
+
 module.exports = eventBus;
