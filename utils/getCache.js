9de9b52e90eba2bb040897e91052437243ad5bfb
Cactus <ndementev56@gmail.com>
2026-05-02 12:42:59 +0300

    cache decorator now catches errors

--- utils/getCache.js
+++ utils/getCache.js
@@ -5,20 +5,23 @@ function getCache(fn, cache, cacheLife) {
     return async function (...args) {
         const data = args[0];
         const cached = cache.get(data);
-    
+
         if (cached) {
             const age = Date.now() - cached.timestamp;
             if (age < cacheLife) {
-                logger.info(`[Cache] Знайдено кешовані дані для ${data} (${(age / 1000).toFixed(1)}s)`);
+                logger.info(`[Cache] Знайдено кеш для ${data} (${(age / 1000).toFixed(1)}s)`);
                 return cached.data;
-            } else {
-                cache.delete(data);
             }
         }
-        
-        const result = await fn(...args);
-        eventBus.emit('profile_ready', { username: data, responseData: result, cache: cache });
-        return result;
+
+        try {
+            const result = await fn(...args);
+            eventBus.emit('profile_ready', { username: data, responseData: result, cache: cache });
+            return result;
+        } catch (error) {
+            logger.error(`[Cache] Помилка при отриманні кешу:', ${error.message}`);
+            throw error;
+        }
     }
- }
+}
 module.exports = getCache;
